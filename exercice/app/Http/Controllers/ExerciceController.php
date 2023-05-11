f2dca9924a000b64f9d4a480f329bb3be39ff254
e2194727Xavier <e2194727@cmaisonneuve.qc.ca>
2023-05-10 21:45:09 -0400

    Commit pour ajouter le travail sur git.

---
+++ exercice/app/Http/Controllers/ExerciceController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ExerciceController extends Controller
+{
+    public function index(){
+        return view('index');
+    }
+    public function index(){
+        return view('index');
+    }
+    public function index(){
+        return view('index');
+    }
+    public function index(){
+        return view('index');
+    }
+}
